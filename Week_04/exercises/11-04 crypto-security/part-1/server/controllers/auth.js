7c90a6ad2c6d28ecfd5d6f33f4a119ade10077a0
Tim Washington <taw@tehmwashington.com>
2021-11-04 19:49:30 -0500

    11-04 late night push

--- Week_04/exercises/11-04 crypto-security/part-1/server/controllers/auth.js
+++ Week_04/exercises/11-04 crypto-security/part-1/server/controllers/auth.js
@@ -1,21 +1,36 @@
+const bcrypt = require('bcryptjs');
 const users = []
 
 module.exports = {
-    login: (req, res) => {
-      console.log('Logging In User')
-      console.log(req.body)
-      const { username, password } = req.body
-      for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
+  login: (req, res) => {
+    const { username, password } = req.body
+    
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].username === username) {
+        const authenticated = bcrypt.compareSync(password, users[i].passwordHash)
+        if (authenticated) {
+          let userToReturn = {...users[i]}
+          delete userToReturn.passwordHash
+          res.status(200).send(userToReturn)
         }
       }
-      res.status(400).send("User not found.")
-    },
-    register: (req, res) => {
-        console.log('Registering User')
-        console.log(req.body)
-        users.push(req.body)
-        res.status(200).send(req.body)
     }
+    res.status(400).send("User not found.")
+  },
+  register: (req, res) => {
+      const { username, email, firstName, lastName, password } = req.body
+      const salt = bcrypt.genSaltSync(5)
+      const passwordHash = bcrypt.hashSync(password, salt)
+      let user = {
+        username,
+        email,
+        firstName,
+        lastName,
+        passwordHash
+      }
+      users.push(user)
+      let userToReturn = {...user}
+      delete userToReturn.passwordHash
+      res.status(200).send(userToReturn)
+  }
 }
